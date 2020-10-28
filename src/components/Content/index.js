909a7c98c221022619957fc8b25bd49723280515
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-10-28 20:59:11 +0700

    up code

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Breadcrumb } from "antd";
-import { Button, Tooltip, Row, Col } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { Breadcrumb, Badge } from "antd";
+import { Button, Tooltip, Row, Col, Card } from "antd";
+import { PlusOutlined, FieldTimeOutlined } from "@ant-design/icons";
+import moment from "moment";
 import "antd/dist/antd.css";
 import "./index.css";
-function Content() {
+function Content({ boardList }) {
   return (
     <div className="content">
       <div>
@@ -23,50 +24,44 @@ function Content() {
       <div>
         <p className="text-myboard">My Board</p>
       </div>
-      <div className='list-boards'>
+      <div className="list-boards">
         <Row>
-        <Col span={5} style={{ padding: '15px' }}>
+          <Col span={5} style={{ padding: "15px" }}>
             <div className="box-add">
-              <Tooltip title="search">
+              <Tooltip title="Add Board">
                 <Button type="primary" shape="circle" icon={<PlusOutlined />} />
               </Tooltip>
             </div>
           </Col>
-          <Col span={5} style={{ padding: '15px' }}>
-            <div className="box-add">
-              <Tooltip title="search">
-                <Button type="primary" shape="circle" icon={<PlusOutlined />} />
-              </Tooltip>
-            </div>
-          </Col>
-          <Col span={5} style={{ padding: '15px' }}>
-            <div className="box-add">
-              <Tooltip title="search">
-                <Button type="primary" shape="circle" icon={<PlusOutlined />} />
-              </Tooltip>
-            </div>
-          </Col>
-          <Col span={5} style={{ padding: '15px' }}>
-            <div className="box-add">
-              <Tooltip title="search">
-                <Button type="primary" shape="circle" icon={<PlusOutlined />} />
-              </Tooltip>
-            </div>
-          </Col>
-          <Col span={5} style={{ padding: '15px' }}>
-            <div className="box-add">
-              <Tooltip title="search">
-                <Button type="primary" shape="circle" icon={<PlusOutlined />} />
-              </Tooltip>
-            </div>
-          </Col>
-          <Col span={5} style={{ padding: '15px' }}>
-            <div className="box-add">
-              <Tooltip title="search">
-                <Button type="primary" shape="circle" icon={<PlusOutlined />} />
-              </Tooltip>
-            </div>
-          </Col>
+          {boardList.map((item) => {
+            return (
+              <Col span={5} key={item.id} style={{ padding: "15px" }}>
+                <div className="board-user">
+                  <Card title={item.boardname} bordered={true}>
+                    <FieldTimeOutlined />{" "}
+                    {moment(item.created).format("YYYY/MM/DD")}
+                    <div>
+                      <Badge
+                        className="site-badge-count"
+                        count={1}
+                        style={{ backgroundColor: "#00FF00" }}
+                      />
+                      <Badge
+                        className="site-badge-count"
+                        count={1}
+                        style={{ backgroundColor: "#FF0000" }}
+                      />
+                      <Badge
+                        className="site-badge-count-4"
+                        count={1}
+                        style={{ backgroundColor: "#FF00FF" }}
+                      />
+                    </div>
+                  </Card>
+                </div>
+              </Col>
+            );
+          })}
         </Row>
       </div>
     </div>
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Layout, Menu, Breadcrumb } from "antd";
 import "./index.css";
 import 'antd/dist/antd.css';
 import Header from '../../components/Header'
 import Content from "../../components/Content";
+import axios from 'axios'
 
 function HomePage() {
+  const [boardList, setBoardList] = useState([]);
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://localhost:4000/board',
+    })
+      .then(function (response) {
+        setBoardList(response.data);
+      });
+  }, []);
   return (
     <div className='home'>
         <Header/>
-        <Content/>
+        <Content boardList={boardList}/>
     </div>
   );
 }
